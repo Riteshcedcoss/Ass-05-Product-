60094384b5185050d8540ea2cce5ddd1b77abf5e
Riteshcedcoss <riteshkumargupta@cedcoss.com>
2022-02-08 11:27:29 +0530

    Products

--- src/index.js
+++ src/index.js
@@ -1,23 +1,54 @@
-function addRow()
-{
-    
-    var fname = document.getElementById('fname').value;
-     var lname = document.getElementById('lname').value;
-      var age = document.getElementById('age').value;
-      
-     
-      var table = document.getElementsByTagName('table')[0];
-      
-    
-      var newRow = table.insertRow(table.rows.length/2+1);
-      
-    
-      var cel1 = newRow.insertCell(0);
-      var cel2 = newRow.insertCell(1);
-      var cel3 = newRow.insertCell(2);
-      
-     
-      cel1.innerHTML = fname;
-      cel2.innerHTML = lname;
-      cel3.innerHTML = age;
+const arrProduct = []; //array of products
+
+//   add product function
+function addProduct() {
+    var id = document.getElementById("pid").value;
+    var name = document.getElementById("pname").value;
+    var price = document.getElementById("price").value;
+
+
+
+    // validator for empty fields
+    if (id && name && price) {
+
+        // validator for numeric input
+        if (isNaN(price)) {
+            document.getElementById('error').style.display = "block";
+            document.getElementById('price').style.borderColor = 'red';
+            document.getElementById('error').innerHTML = '*Enter numeric value in price.'
+            return;
+        }
+
+
+        document.getElementById('error').style.display = "none";
+        document.getElementById('price').style.border = '0.5px solid black';
+
+        arrProduct.push({ id: id, name: name, price: price }); //adding elements to array
+        addElement(arrProduct);
+    } else {
+
+        // if text fields are empty
+        document.getElementById('error').style.display = "block";
+        document.getElementById('error').innerHTML = '*Please fill all the details.'
+
+
+    }
+}
+
+//   add element function to add element in table
+function addElement(arr) {
+    var table =
+        "<table> <tr><th>Product Id</th><th>Product Name</th><th>Price</th></tr> ";
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+        table +=
+            "<tr><td>" +
+            arr[i].id +
+            "</td><td>" +
+            arr[i].name +
+            "</td><td>" +
+            `USD ${arr[i].price}` +
+            "</td></tr>";
+    }
+    document.getElementById("product").innerHTML = table + "</table>";
 }
