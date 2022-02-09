241fce0945092fb2eb2c680cb3805ef17b9efea5
Riteshcedcoss <riteshkumargupta@cedcoss.com>
2022-02-09 17:33:03 +0530

    Product Add

--- src/index.js
+++ src/index.js
@@ -1,79 +1,29 @@
-products = [];
-function isValidateForm(id, name, price) {
-  errors = [];
-  if (!id) {
-    errors.push(alert("Id field is empty."));
-  }
-  if (!name) {
-    errors.push(alert("Name field is empty."));
-  }
-  if (!price) {
-    errors.push(alert("Price field is empty."));
-  }
+var group=[];
+function submitform(){
+    var prId=document.getElementById('product_Id').value;
+    var prName=document.getElementById('product_name').value;
+    var prPrice=document.getElementById('product_price').value;
+    
 
-  return errors;
-}
-function submitForm() {
-  var id = document.getElementById("id").value;
-  var name = document.getElementById("name").value;
-  var price = document.getElementById("price").value;
-  if (isValidateForm(id, name, price).length == 0) {
-    if (NotduplicateId(id)) {
-      p = {
-        id: id,
-        name: name,
-        price: price,
-      };
-      products.push(p);
-      display();
-    }
-  }
-}
-function NotduplicateId(id) {
-  for (let i = 0; i < products.length; i++) {
-    if (products[i].id == id) {
-      alert("Product with this id is already in database");
-      return false;
-    }
-  }
-  return true;
-}
-function display() {
-  var tblBody = document.createElement("tbody");
-  for (let i = 0; i < products.length; i++) {
-    var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    cell1.innerText = products[i].id;
-    cell2.innerText = products[i].name;
-    cell3.innerText = products[i].price;
-    cell4.innerHTML =
-      '<a href="#" onclick="editForm(' + products[i].id + ')">Edit</a>';
-    tblBody.appendChild(row);
-  }
-  var old_tbody = document.getElementById("tbody");
-  if (old_tbody == null) {
-    document.getElementById("table").appendChild(tblBody);
-  } else {
-    document
-      .getElementById("tbody")
-      .parentNode.replaceChild(tblBody, old_tbody);
-  }
 
-  tblBody.setAttribute("id", "tbody");
+    var object={};
+    object.prId=prId;
+    object.prName=prName;
+    object.prPrice=prPrice;
+
+    group.push(object);
+
+    display();
+    console.log("Group".group);
 }
-function editForm(id) {
-  for (var i = 0; i < products.length; i++) {
-    if (products[i].id == id) {
-      break;
+
+function display(){
+    var html='';
+    for(i=0;i<group.length;i++){
+        html+='<tr><td>'+group[i].prId+'</td><td>'+group[i].prName + '</td> <td>' +group[i].prPrice +'</td></tr>';
+        html +='</table>';
+        document.getElementById('table_body').innerHTML=html;
     }
-  }
-  document.getElementById("id").value = products[i].id;
-  document.getElementById("id").readOnly = true;
-  document.getElementById("id").style.border = "thick solid blue";
-  document.getElementById("id").value = products[i].id;
-  document.getElementById("id").value = products[i].id;
 }
-function update() {}
+
+
